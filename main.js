66f6ad570b2f726a96c3288c4a3e25f821a05450
maan1384 <35119249+maan1384@users.noreply.github.com>
2018-02-27 16:28:30 -0800

    created simple layout

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
 // document.getElementById('counter').innerHTML = "hello";
 
 //initialize - set-up variables
-var counter = document.getElementById('counter');
-var fromTop = window.pageYOffset;
+var board = document.getElementById('board');
+var fromLeft = window.pageXOffset;
 
 
-counter.innerHTML = "counter text";
+board.innerHTML = "SCROLL TO BEGIN";
 
 //pseudocode - when the user is scrolling update the variable fromTop and make decisions to change stuff on the pageYOffset
 //window.scroll<- boolean (yes/no)
 window.onscroll = function() {
   //when the user scrolls do all of this...
 
-  fromTop = window.pageYOffset
+  fromLeft = window.pageXOffset
   //test if it works
-  console.log(fromTop);
+  console.log(fromLeft);
 
-  counter.style.top = 100 + fromTop + "px";
+  board.style.left = 100 + fromLeft + "px";
 
-  if (fromTop > 1000){
-    counter.innerHTML = "more than 1000 leagues";
-    counter.style.backgroundColor="red";
-    counter.style.backgroundImage="url(yellowsubmarine.png)";
+  if (fromLeft > 0){
+    board.innerHTML = "";
+    board.style.backgroundImage="url(images/skate.png)";
   } else {
-    counter.innerHTML = "counter text";
-    counter.style.backgroundColor="green";
-    counter.style.backgroundImage= "url(yellowsubmarine.png)";
+    board.innerHTML = "SCROLL TO BEGIN";
+    board.style.backgroundImage= "url(skateboard.png)";
   }
 
 
